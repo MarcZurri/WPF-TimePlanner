1f42101568f956fcfcc2b53ae5cc1dff4e7e169f
Marc Zurriaga Mitjans <m.zurriaga.mitjans@avanade.com>
2025-09-29 16:49:27 +0200

    Improved TestApp

---
+++ ZTimePlanner.ControlsTestApp/Converters/PlannerTypeBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using ZTimePlanner.Controls.Models;
+
+namespace ZTimePlanner.ControlsTestApp.Converters
+{
+    [ValueConversion(typeof(PlannerTypes), typeof(bool))]
+    public class PlannerTypeBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is PlannerTypes && !String.IsNullOrEmpty(value.ToString()) && parameter is string && !String.IsNullOrEmpty(parameter.ToString()))
+                return value.ToString() == parameter.ToString();
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (value is bool) && (bool)value ? parameter : null;
+        }
+    }
+}
--- ZTimePlanner.ControlsTestApp/MainWindow.xaml.cs
+++ ZTimePlanner.ControlsTestApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using ZTimePlanner.Controls.Models;
 
 namespace ZTimePlanner.ControlsTestApp
 {
@@ -7,6 +8,18 @@ namespace ZTimePlanner.ControlsTestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public PlannerTypes? PlannerType
+        {
+            get { return this.plannerType; }
+            set
+            {
+                if (value != null)
+                    this.plannerType = (PlannerTypes)value;
+            }
+        }
+
+        private PlannerTypes plannerType = PlannerTypes.WorkWeek;
+
         public List<string> RowHeaders = new List<string>()
         {
         "1","2","3","4","5","6","7","8","9","10",
@@ -15,6 +28,7 @@ namespace ZTimePlanner.ControlsTestApp
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
 
             //this.planner.RowHeaderItemsSource = this.RowHeaders;
         }
@@ -22,11 +36,8 @@ namespace ZTimePlanner.ControlsTestApp
         private void PlannerTypeChecked(object sender, RoutedEventArgs e)
         {
             var radioButton = sender as System.Windows.Controls.RadioButton;
-            if (this.planner != null && radioButton != null && radioButton.Tag != null)
-            {
-                if (Enum.TryParse(radioButton.Tag.ToString(), out ZTimePlanner.Controls.Models.PlannerTypes plannerType))
-                    this.planner.PlannerType = plannerType;
-            }
+            if (this.planner != null && this.PlannerType != null && this.planner.PlannerType != this.PlannerType)
+                this.planner.PlannerType = this.PlannerType.Value;
         }
     }
 }
